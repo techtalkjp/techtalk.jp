58e7c5aea65de9e5cf8979e41340078c3973f7d1
coji <coji@techtalk.jp>
2023-07-23 22:33:46 +0900

    bump

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,16 @@ module.exports = {
     '@remix-run/eslint-config/node',
     'prettier',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
   plugins: ['chakra-ui'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: { '@typescript-eslint/no-floating-promises': 'error' },
+    },
+  ],
 }
