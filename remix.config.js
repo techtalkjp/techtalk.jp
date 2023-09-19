617b16c996634408fdae0215f1e66d7550078da6
coji <coji@techtalk.jp>
2023-09-19 09:11:54 +0900

    chore: remix v2 remix

--- remix.config.js
+++ remix.config.js
@@ -1,16 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   ignoredRouteFiles: ['**/.*'],
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    v2_headers: true,
-    v2_dev: true,
-  },
-  tailwind: true,
-  postcss: true,
-  serverModuleFormat: 'cjs',
-  serverDependenciesToBundle: 'all',
+  serverModuleFormat: 'esm',
 }
