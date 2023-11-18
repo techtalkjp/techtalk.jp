766b0fba443bf256166e2bf91854bcd191372ddd
coji <coji@techtalk.jp>
2023-11-18 23:14:28 +0900

    Delete unused files and configurations

--- old/app/entry.server.tsx
+++
@@ -1,7 +0,0 @@
-import { RemixServer } from '@remix-run/react'
-import { handleRequest } from '@vercel/remix'
-
-export default function (request: Request, responseStatusCode: number, responseHeaders: Headers, remixContext: any) {
-  const remixServer = <RemixServer context={remixContext} url={request.url} />
-  return handleRequest(request, responseStatusCode, responseHeaders, remixServer)
-}
--- old/prettier.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 120,
-  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
-}
