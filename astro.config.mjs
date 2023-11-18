346832ae09f91ac32905964d26e6e2c6a485e742
coji <coji@techtalk.jp>
2023-11-18 15:34:43 +0900

    Remove unused dependencies and update body background color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   experimental: {
     i18n: {
@@ -11,12 +9,5 @@ export default defineConfig({
       locales: ["ja", "en"],
     },
   },
-  output: "server",
-  adapter: cloudflare(),
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
