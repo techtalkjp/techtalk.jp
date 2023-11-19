d3eaff7c38ced4db573037ffccea4bd350bf3eb7
coji <coji@techtalk.jp>
2023-11-19 23:39:12 +0900

    Remove unused code and update tsconfig.json

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="astro/client" />
-
-declare namespace astroHTML.JSX {
-  interface HTMLAttributes {
-    popover?: boolean
-    anchor?: string
-  }
-
-  interface ButtonHTMLAttributes {
-    popovertarget?: string
-  }
-}
