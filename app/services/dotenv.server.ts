ff001e8ea3c1f3eb5b01ff8d40323f3af8e6ea04
coji <coji@techtalk.jp>
2024-02-02 14:50:37 +0900

    Add dotenv server file

--- app/services/dotenv.server.ts
+++ app/services/dotenv.server.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod'
 
+console.log('dotenv')
 const envSchema = z.object({
   SLACK_WEBHOOK: z.string(),
   SENDGRID_API_KEY: z.string(),
 })
-
 envSchema.parse(process.env)
 
 declare global {
