24955b7910383871689a20f4fb10d21f7cec7850
coji <coji@techtalk.jp>
2024-02-15 22:29:32 +0900

    Refactor Menubar in demo.tsx

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -12,30 +12,38 @@ export default function TestPage() {
   const menu = location.pathname.split('/')[2]
 
   return (
-    <div className="m-4 grid grid-cols-1 gap-4">
-      <Menubar className="shadow-none">
-        {Object.keys(demoPages).map((demoMenu) => (
-          <MenubarMenu key={demoMenu}>
-            <MenubarTrigger
-              className={cn(
-                'capitalize',
-                demoMenu === menu && 'underline decoration-primary decoration-2 underline-offset-4',
-              )}
-            >
-              {demoMenu}
-            </MenubarTrigger>
-            <MenubarContent>
-              {demoPages[demoMenu].map(({ path, title }) => (
-                <MenubarItem key={path} asChild>
-                  <Link to={path}>{title}</Link>
-                </MenubarItem>
-              ))}
-            </MenubarContent>
-          </MenubarMenu>
-        ))}
-      </Menubar>
+    <div className="grid h-screen grid-cols-1 grid-rows-[auto_1fr_auto] gap-4">
+      <header>
+        <h1 className="mx-4 my-2 text-2xl font-bold">TechTalk demos</h1>
+        <Menubar className="rounded-none border-b border-l-0 border-r-0 border-t shadow-none">
+          {Object.keys(demoPages).map((demoMenu) => (
+            <MenubarMenu key={demoMenu}>
+              <MenubarTrigger
+                className={cn(
+                  'capitalize',
+                  demoMenu === menu &&
+                    'relative after:absolute after:bottom-0 after:left-2 after:right-2 after:block after:h-1 after:rounded-md after:bg-primary',
+                )}
+              >
+                {demoMenu}
+              </MenubarTrigger>
+              <MenubarContent>
+                {demoPages[demoMenu].map(({ path, title }) => (
+                  <MenubarItem key={path} asChild>
+                    <Link to={path}>{title}</Link>
+                  </MenubarItem>
+                ))}
+              </MenubarContent>
+            </MenubarMenu>
+          ))}
+        </Menubar>
+      </header>
 
-      <Outlet />
+      <main className="m-4">
+        <Outlet />
+      </main>
+
+      <footer className="px-4 py-2 text-center">Copyright&copy; TechTalk inc.</footer>
     </div>
   )
 }
