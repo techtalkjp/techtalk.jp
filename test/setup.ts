4b9e67986937abff83e89923ac1cbe57b719d80c
coji <coji@techtalk.jp>
2024-04-05 08:25:38 +0900

    Update schema version in biome.json and modify validate script in package.json

--- test/setup.ts
+++ test/setup.ts
@@ -8,5 +8,6 @@ class MockResizeObserver {
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 ;(global as any).ResizeObserver = MockResizeObserver
+
 process.env.SLACK_WEBHOOK =
   'https://hooks.slack.com/services/TEST_SLACK_WEBHOOK'
