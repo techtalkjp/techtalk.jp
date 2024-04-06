c85a4d50e75caa9bdb74df01568984a8416bdfbc
coji <coji@techtalk.jp>
2024-04-06 21:49:48 +0900

    Update AWS SDK dependencies and refactor image upload services

--- app/routes/demo+/conform.image-upload/route.tsx
+++ app/routes/demo+/conform.image-upload/route.tsx
@@ -1,7 +1,18 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import dayjs from 'dayjs'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
 import {
+  json,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+} from '@remix-run/node'
+import { Form, useActionData, useLoaderData } from '@remix-run/react'
+import dayjs from 'dayjs'
+import { z } from 'zod'
+import {
+  Button,
+  Input,
+  Label,
+  Stack,
   Table,
   TableBody,
   TableCell,
@@ -9,46 +20,92 @@ import {
   TableHeader,
   TableRow,
 } from '~/components/ui'
-import { ImageEndpointUrl, list } from './services/r2'
+import { ImageEndpointUrl, list, upload } from './services/r2.server'
+
+const schema = z.object({
+  file: z.instanceof(File),
+})
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const objects = await list()
   return json({ objects, ImageEndpointUrl })
 }
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const submission = parseWithZod(await request.formData(), { schema })
+  if (submission.status !== 'success') {
+    return json({ lastResult: submission.reply() })
+  }
+
+  await upload(submission.value.file)
+  return json({ lastResult: submission.reply({ resetForm: true }) })
+}
+
 export default function ImageUploadDemoPage() {
   const { objects, ImageEndpointUrl } = useLoaderData<typeof loader>()
+  const actionData = useActionData<typeof action>()
+  const [form, { file }] = useForm({
+    lastResult: actionData?.lastResult,
+    onValidate: ({ formData }) => parseWithZod(formData, { schema }),
+  })
 
   return (
-    <div>
-      <h1>Image Upload Demo</h1>
-      <p>Upload an image</p>
+    <Stack>
+      <Stack asChild>
+        <Form
+          method="POST"
+          encType="multipart/form-data"
+          {...getFormProps(form)}
+        >
+          <div>
+            <Label htmlFor={file.id}>File</Label>
+            <Input {...getInputProps(file, { type: 'file' })} />
+            <div id={file.errorId} className="text-destructive">
+              {file.errors}
+            </div>
+          </div>
 
-      <Table>
-        <TableHeader>
-          <TableHead>Key</TableHead>
-          <TableHead>LastModified</TableHead>
-          <TableHead>Size</TableHead>
-        </TableHeader>
-        <TableBody>
-          {objects?.map((object) => (
-            <TableRow key={object.Key}>
-              <TableCell>
-                {object.Key}
-                <img
-                  src={`${ImageEndpointUrl}${object.Key}`}
-                  alt={object.Key}
-                />
-              </TableCell>
-              <TableCell>{dayjs(object.LastModified).format()}</TableCell>
-              <TableCell>
-                {object.Size?.toLocaleString()}
-                <small> bytes</small>
-              </TableCell>
+          <Button type="submit">Upload</Button>
+        </Form>
+      </Stack>
+
+      <div className="overflow-auto rounded border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Image</TableHead>
+              <TableHead>LastModified</TableHead>
+              <TableHead>Size</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {objects?.map((object) => (
+              <TableRow key={object.Key}>
+                <TableCell>
+                  <div className="relative">
+                    <span className="absolute bottom-2 right-2 text-sm text-white drop-shadow">
+                      {object.Key}
+                    </span>
+                    <img
+                      className="rounded"
+                      loading="lazy"
+                      src={`${ImageEndpointUrl}${object.Key}`}
+                      alt={object.Key}
+                    />
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {dayjs(object.LastModified).format('YYYY-MM-DD HH:mm')}
+                </TableCell>
+                <TableCell>
+                  {object.Size?.toLocaleString()}
+                  <small> bytes</small>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Stack>
   )
 }
---
+++ app/routes/demo+/conform.image-upload/services/r2.server.ts
@@ -0,0 +1,46 @@
+import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+import { Readable } from 'node:stream'
+
+const BUKET_NAME = 'techtalk' as const
+
+const client = new S3Client({
+  endpoint: process.env.R2_ENDPOINT_URL,
+  region: 'auto',
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+  },
+})
+
+export const ImageEndpointUrl = process.env.IMAGE_ENDPOINT_URL
+
+export const list = async () => {
+  const { Contents } = await client.send(
+    new ListObjectsV2Command({ Bucket: BUKET_NAME, MaxKeys: 100 }),
+  )
+
+  return Contents
+}
+
+export const upload = async (file: File) => {
+  const { name, type } = file
+
+  const stream = new Readable({ read: () => {} })
+  stream.push(Buffer.from(await file.arrayBuffer()))
+  stream.push(null)
+
+  const upload = new Upload({
+    client,
+    params: {
+      Body: stream,
+      Bucket: BUKET_NAME,
+      Key: name,
+    },
+    // 性能改善用の細かなパラメータ
+    queueSize: 10, // アップロードの並列数
+    partSize: 1024 * 1024 * 5, // 1パート当たりのサイズ
+  })
+
+  return await upload.done()
+}
