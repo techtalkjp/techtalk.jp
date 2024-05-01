24a44c3560e71d0bf1ebd637e76f4d9e2d1f3f86
coji <coji@techtalk.jp>
2024-05-01 22:06:18 +0900

    Update dependencies for @aws-sdk/client-s3, @aws-sdk/lib-storage, @aws-sdk/s3-request-presigner, @mdxeditor/editor, isbot, and zod packages

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,5 @@
-import type { AppLoadContext, EntryContext } from '@remix-run/node'
-import { createReadableStreamFromReadable } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
-import { isbot } from 'isbot'
-import { PassThrough } from 'node:stream'
-import { renderToPipeableStream } from 'react-dom/server'
+import { handleRequest, type EntryContext } from '@vercel/remix'
 import { createSitemapGenerator } from 'remix-sitemap'
 
 const { isSitemapUrl, sitemap } = createSitemapGenerator({
@@ -11,131 +7,21 @@ const { isSitemapUrl, sitemap } = createSitemapGenerator({
   generateRobotsTxt: true,
 })
 
-const ABORT_DELAY = 5_000
-
-export default async function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
-  loadContext: AppLoadContext,
-) {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  if (isSitemapUrl(request)) return await sitemap(request, remixContext as any)
-
-  return isbot(request.headers.get('user-agent'))
-    ? handleBotRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext,
-      )
-    : handleBrowserRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext,
-      )
-}
-
-function handleBotRequest(
+export default async function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  let statusCode = responseStatusCode
-  return new Promise((resolve, reject) => {
-    let shellRendered = false
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
-      {
-        onAllReady() {
-          shellRendered = true
-          const body = new PassThrough()
-          const stream = createReadableStreamFromReadable(body)
-
-          responseHeaders.set('Content-Type', 'text/html')
-
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: statusCode,
-            }),
-          )
-
-          pipe(body)
-        },
-        onShellError(error: unknown) {
-          reject(error)
-        },
-        onError(error: unknown) {
-          statusCode = 500
-          // Log streaming rendering errors from inside the shell.  Don't log
-          // errors encountered during initial shell rendering since they'll
-          // reject and get logged in handleDocumentRequest.
-          if (shellRendered) {
-            console.error(error)
-          }
-        },
-      },
-    )
-
-    setTimeout(abort, ABORT_DELAY)
-  })
-}
-
-function handleBrowserRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
-) {
-  let statusCode = responseStatusCode
-  return new Promise((resolve, reject) => {
-    let shellRendered = false
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
-      {
-        onShellReady() {
-          shellRendered = true
-          const body = new PassThrough()
-          const stream = createReadableStreamFromReadable(body)
-
-          responseHeaders.set('Content-Type', 'text/html')
-
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: statusCode,
-            }),
-          )
-
-          pipe(body)
-        },
-        onShellError(error: unknown) {
-          reject(error)
-        },
-        onError(error: unknown) {
-          statusCode = 500
-          // Log streaming rendering errors from inside the shell.  Don't log
-          // errors encountered during initial shell rendering since they'll
-          // reject and get logged in handleDocumentRequest.
-          if (shellRendered) {
-            console.error(error)
-          }
-        },
-      },
-    )
-
-    setTimeout(abort, ABORT_DELAY)
-  })
+  if (isSitemapUrl(request)) {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    return await sitemap(request, remixContext as any)
+  }
+  const remixServer = <RemixServer context={remixContext} url={request.url} />
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer,
+  )
 }
