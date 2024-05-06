d576f0ddca854156a10a8b478f1a92c5e5407c22
coji <coji@techtalk.jp>
2024-05-07 08:55:56 +0900

    Refactor Select component to handle optional empty values in onValueChange

--- app/routes/demo+/conform.select.tsx
+++ app/routes/demo+/conform.select.tsx
@@ -24,7 +24,7 @@ import {
 
 const schema = z.object({
   type: z.enum(['inside-form', 'outside-form']),
-  option: z.enum(['option1', 'option2']),
+  option: z.enum(['option1', 'option2']).optional(),
 })
 
 export const action = async ({ request }: ActionFunctionArgs) => {
