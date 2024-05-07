373b13b55d48f48a97aeab897ace27c423067b97
coji <coji@techtalk.jp>
2024-05-07 11:06:51 +0900

    Refactor form input names in InsideForm and OutsideForm components

--- app/routes/demo+/conform.select/forms/inside-form.tsx
+++ app/routes/demo+/conform.select/forms/inside-form.tsx
@@ -37,7 +37,7 @@ export const InsideForm = () => {
 
   return (
     <Form method="POST" {...getFormProps(form)}>
-      <input name="formType" value={FormType.INSIDE_FORM} />
+      <input name="formType" value={FormType.INSIDE_FORM} type="hidden" />
       <input
         name={option.name}
         value={option.value}
