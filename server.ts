eb471d303a65aff93723bee861147cc2e581f898
coji <coji@techtalk.jp>
2024-10-13 18:41:19 +0900

    Refactor server.ts to use react-router for request handling

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { createRequestHandler, type ServerBuild } from 'react-router'
+// @ts-ignore This file won’t exist if it hasn’t yet been built
 import * as build from './build/server'
 import { getLoadContext } from './load-context'
 
