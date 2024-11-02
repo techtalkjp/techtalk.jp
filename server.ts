20afc15f6ba772decd50629bc8c3449c9ce0970a
coji <coji@techtalk.jp>
2024-11-02 23:18:18 +0900

    Refactor server.ts to wrap fetch handler in runSession for improved session management

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { createRequestHandler, type ServerBuild } from 'react-router'
+import { runSession } from 'session-context'
 // @ts-ignore This file won’t exist if it hasn’t yet been built
 import * as build from './build/server'
 import { getLoadContext } from './load-context'
@@ -6,32 +7,35 @@ import { getLoadContext } from './load-context'
 const requestHandler = createRequestHandler(build as unknown as ServerBuild)
 
 export default {
+  // biome-ignore lint/suspicious/useAwait: <explanation>
   async fetch(request, env, ctx) {
-    try {
-      const loadContext = getLoadContext({
-        request,
-        context: {
-          cloudflare: {
-            // This object matches the return value from Wrangler's
-            // `getPlatformProxy` used during development via Remix's
-            // `cloudflareDevProxyVitePlugin`:
-            // https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy
-            // @ts-ignore
-            cf: request.cf,
-            ctx: {
-              waitUntil: ctx.waitUntil.bind(ctx),
-              passThroughOnException: ctx.passThroughOnException.bind(ctx),
+    return runSession(async () => {
+      try {
+        const loadContext = getLoadContext({
+          request,
+          context: {
+            cloudflare: {
+              // This object matches the return value from Wrangler's
+              // `getPlatformProxy` used during development via Remix's
+              // `cloudflareDevProxyVitePlugin`:
+              // https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy
+              // @ts-ignore
+              cf: request.cf,
+              ctx: {
+                waitUntil: ctx.waitUntil.bind(ctx),
+                passThroughOnException: ctx.passThroughOnException.bind(ctx),
+              },
+              caches,
+              env,
             },
-            caches,
-            env,
           },
-        },
-      })
+        })
 
-      return await requestHandler(request, loadContext)
-    } catch (error) {
-      console.log(error)
-      return new Response('An unexpected error occurred', { status: 500 })
-    }
+        return await requestHandler(request, loadContext)
+      } catch (error) {
+        console.log(error)
+        return new Response('An unexpected error occurred', { status: 500 })
+      }
+    })
   },
 } satisfies ExportedHandler<Env>
