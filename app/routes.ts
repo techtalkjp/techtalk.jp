4c43e837a3a24bbebacb317f88daf2e13fa9f342
coji <coji@techtalk.jp>
2024-11-22 21:49:27 +0900

    feat: add react-router configuration and update dependencies

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,6 @@
-import type { RouteConfig } from '@react-router/dev/routes'
 import { remixRoutesOptionAdapter } from '@react-router/remix-routes-option-adapter'
 import { flatRoutes } from 'remix-flat-routes'
 
-export const routes: RouteConfig = remixRoutesOptionAdapter((defineRotue) =>
+export default remixRoutesOptionAdapter((defineRotue) =>
   flatRoutes('routes', defineRotue),
 )
