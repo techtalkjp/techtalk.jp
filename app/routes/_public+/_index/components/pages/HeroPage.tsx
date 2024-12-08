52ba9b467dd7d3e6fddb28ab0b6ea54d84d8bc4a
coji <coji@techtalk.jp>
2024-12-08 12:36:19 +0900

    feat: update hero text for improved clarity and impact in English and Japanese

--- app/routes/_public+/_index/components/pages/HeroPage.tsx
+++ app/routes/_public+/_index/components/pages/HeroPage.tsx
@@ -8,16 +8,12 @@ export const HeroPage = () => {
   return (
     <CoverPage id="hero" bgImage="/images/hero.webp" className="text-center">
       <Heading size="5xl">
-        <TextReveal text="Technology enables," />
-        <TextReveal
-          text="but value differentiates."
-          className="text-primary"
-          delay={0.5}
-        />
+        <TextReveal text="Tech talks," />
+        <TextReveal text="Value matters" className="text-primary" delay={0.5} />
       </Heading>
       <TextReveal
         className="text-lg"
-        text={t('hero.subcopy', '技術を語り、価値を創る')}
+        text="技術を語り、価値を創る。"
         delay={2}
         isLastLine
       />
