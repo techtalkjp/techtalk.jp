c9294c7a43fc72914448eb3ff8e882f0f90981b6
coji <coji@techtalk.jp>
2025-03-02 20:31:01 +0900

    fix: enhance link styling for improved visibility and user experience

--- app/routes/demo+/about/route.tsx
+++ app/routes/demo+/about/route.tsx
@@ -5,7 +5,12 @@ export default function About() {
       <div>溝口 浩二 (coji) </div>
 
       <div>
-        <a href="https://zenn.dev/coji" target="_blank" rel="noreferrer">
+        <a
+          href="https://zenn.dev/coji"
+          target="_blank"
+          rel="noreferrer"
+          className="text-primary hover:underline"
+        >
           https://zenn.dev/coji
         </a>
       </div>
