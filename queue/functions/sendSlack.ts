5974670d99a86b7f5ce67344cb58a7f8b5b97411
coji <coji@techtalk.jp>
2025-03-05 22:43:11 +0900

    fix: await

--- queue/functions/sendSlack.ts
+++ queue/functions/sendSlack.ts
@@ -1,20 +1,9 @@
-import { fromPromise, type ResultAsync } from 'neverthrow'
 import type { ContactFormData } from '~/routes/_public+/api.contact/types'
 
-type SendSlackError = { type: 'SendSlackError'; message: string }
-const sendSlackImpl = async (webhookUrl: string, data: ContactFormData) => {
+export const sendSlack = async (webhookUrl: string, data: ContactFormData) => {
   await fetch(webhookUrl, {
     method: 'POST',
     body: JSON.stringify({ data }),
   })
   return data
 }
-
-export const sendSlack = (
-  webhookUrl: string,
-  data: ContactFormData,
-): ResultAsync<ContactFormData, SendSlackError> =>
-  fromPromise(sendSlackImpl(webhookUrl, data), (e) => ({
-    type: 'SendSlackError',
-    message: String(e),
-  }))
