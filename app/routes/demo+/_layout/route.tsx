2e2bea1edbaeb5525f0da7215d7b291f70038d38
coji <coji@techtalk.jp>
2025-03-31 22:22:45 +0900

    feat: update demo async validation route extension for consistency

--- app/routes/demo+/_layout/route.tsx
+++ app/routes/demo+/_layout/route.tsx
@@ -36,7 +36,7 @@ const demoPages: {
     {
       path: '/demo/conform/async-validation',
       title: '非同期バリデーション',
-      ext: '/route.tsx',
+      ext: '+/_index/route.tsx',
     },
     {
       path: '/demo/conform/image-upload',
